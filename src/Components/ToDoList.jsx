58bfcfb442ddc595748e710cf4b587f71268bb5a
ritishbhardwaj <ritishbhardwaj02@gmail.com>
2024-01-24 21:32:49 +0530

    "major update"

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -14,18 +14,37 @@ export default function ToDoList() {
 
         <tbody>
           <tr>
-            <th className="col-2"> 2</th>
-            <td className="col-4"> hello world</td>
-            <div className="col-6">
+            <th className="col-2"> 1</th>
+            <td className="col-4"> typing diary</td>
+            <td className="col-6">
               <div className="row">
-                <td className="col-8"> <p>coding djnfjsidnfisdnfdfniefbjisdnfsnfiwefniuwefguiwebfijweniwenfiunone</p></td>
+                <div className="col-8">
+                  <p>type your thoughts on laptop's notebook today</p>
+                </div>
                 <div className="col-4">
-                  <button type="button" className="btn btn-success col-4">
-                    delete
+                  <button type="button" className="btn btn-success">
+                    Delete
                   </button>
                 </div>
               </div>
-            </div>
+            </td>
+          </tr>
+          <tr>
+            <th className="col-2"> 2</th>
+            <td className="col-4"> eating breakfast</td>
+            <td className="col-6">
+              <div className="row">
+                <div className="col-8">
+                  <p>eat breakfast at 7:00 am</p>
+                </div>
+                <div className="col-4">
+                  {" "}
+                  <button type="button" className="btn btn-success">
+                    Delete
+                  </button>{" "}
+                </div>
+              </div>
+            </td>
           </tr>
         </tbody>
       </table>
